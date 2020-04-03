73cca8a739c4d03edd6e4d145fde5fb1633c9d57
Galyopa <61745653+Galyopa@users.noreply.github.com>
2020-04-03 19:48:57 +0300

    TDD HW (#4)
    * First commit with HW & Made some func
    
    * Done HW
    
    * Change code with support's comments
    
    * Add changes with new comments
    
    * Remove redundant code
    
    * Use string enumerating & update formatting
    
    * Realised abstract method in class Animal
    
    * Revert "Merge pull request #2 from BilyiVoron/oop_branch"
    
    * fix typo, test rights
    
    * Robot initialisation complete
    
    * Add test Robot's coordinates
    
    * lesson test complete
    
    * TDD HW minimal Done
    
    * refactoring code
    
    Co-authored-by: BilyiVoron <aborsen1072@gmail.com>
    Co-authored-by: Bohdan Dats'ko <53016307+BilyiVoron@users.noreply.github.com>
    Co-authored-by: Illia Sukonnik <illia.sukonnik@made.com>
    Co-authored-by: Illia Sukonnik <illia.sukonnik@gmail.com>

--- lantern/tdd/lonely_robot.py
+++ lantern/tdd/lonely_robot.py
@@ -15,10 +15,34 @@ class Robot:
         if self.y > self.asteroid.y:
             raise MissAsteroidError()
 
+    def check_robot_on_an_asteroid(self):
+        if self.x > self.asteroid.x or self.y > self.asteroid.y or self.x < 0 or self.y < 0:
+            raise RobotFallFromAsteroidError()
+
     def turn_left(self):
-        turns = {"E": "N"}
+        turns = dict(E="N", N="W", W="S", S="E")
         self.direction = turns[self.direction]
+
+    def turn_right(self):
+        turns = dict(N="E", E="S", S="W", W="N")
+        self.direction = turns[self.direction]
+
+    def move_forward(self):
+        move_forward = dict(N=(self.x, self.y + 1), E=(self.x + 1, self.y), S=(self.x, self.y - 1),
+                            W=(self.x - 1, self.y))
+        self.x, self.y = move_forward[self.direction]
+        self.check_robot_on_an_asteroid()
+
+    def move_backward(self):
+        move_backward = dict(N=(self.x, self.y - 1), E=(self.x - 1, self.y), S=(self.x, self.y + 1),
+                             W=(self.x + 1, self.y))
+        self.x, self.y = move_backward[self.direction]
+        self.check_robot_on_an_asteroid()
 
 
 class MissAsteroidError(Exception):
+    pass
+
+
+class RobotFallFromAsteroidError(Exception):
     pass
