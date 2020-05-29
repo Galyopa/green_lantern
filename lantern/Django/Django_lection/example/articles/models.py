067685e8013a362b00fe5b6d08f01ea70fde3a52
Galyopa <galyopa@gmail.com>
2020-05-29 18:34:31 +0300

    Part of second lection

--- lantern/Django/Django_lection/example/articles/models.py
+++ lantern/Django/Django_lection/example/articles/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    id = models.UUIDField(primary_key=True)
+    title = models.CharField(max_length=255, verbose_name='Article')
+    body = models.TextField(max_length=5000)
+    views = models.IntegerField(default=0)
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='articles')
+    tags = models.ManyToManyField(to="Tag", blank=True)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True)
